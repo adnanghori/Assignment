3ee89cb1f931572ce6aca7c516648cdb8cb05e14
adnanghori <adnanghori12@gmail.com>
2021-11-14 11:50:22 +0530

    Final Commit

--- assignment_2.c
+++ assignment_2.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(int argc, char const *argv[]) {
-
+  int number;
+  printf("Enter A Number: ");
+  scanf("%d",&number);
+  printf("Decimal Number Is: %d\n",number);
+  printf("Octal Number Is: %o\n",number);
+  printf("HexaDecimal Number Is: %x\n",number);
+  printf("HexaDecimal Number Is: %X\n",number);
   return 0;
 }
