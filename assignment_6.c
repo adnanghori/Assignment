eb7f9772b0ecfb94b32972bbe5c6834918b2e3bf
adnanghori <adnanghori12@gmail.com>
2021-11-14 13:30:21 +0530

    Intial Commit

---
+++ assignment_6.c
@@ -0,0 +1,10 @@
+/*
+6. Write a program that takes as input the numerator or denominator of two fraction. Your program should display the numerator and denominator of the fraction that represent the product of the two fraction. also display the percentage equivalent of resulting product.
+
+*/
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+
+  
+  return 0;
+}
