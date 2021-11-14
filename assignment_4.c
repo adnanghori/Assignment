fd595a2642493c1c36c50f7833d82f3fb6d343ec
adnanghori <adnanghori12@gmail.com>
2021-11-14 12:09:44 +0530

    Final Commit

---
+++ assignment_4.c
@@ -0,0 +1,10 @@
+/*
+4. Write a program which initializes 3 character variables a,b,c to A,B,C respectively and print their ASCII value.
+
+*/
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+  char a='A',b='B',c='C';
+  printf("ASCII Value Of a:%d\nASCII Value Of b:%d\nASCII Value Of c:%d\n",a,b,c);
+  return 0;
+}
